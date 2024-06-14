388b1a871808d738822cc9d418dddd3aa4625182
AnderFigueroa <20220105@ricaldone.edu.sv>
2024-06-14 15:13:15 -0600

    pantallas de bienvenida

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import OnboardingScreen from './src/screens/OnboardingScreen';
+import Anuncio1Screen from './src/screens/Anuncio1Screen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+const Stack = createStackNavigator();
 
-export default function App() {
+
+const App = () => {
+  const [isAppFirstLaunched, setIsAppFirstLaunched] = React.useState(null);
+
+  React.useEffect(async () => {
+    const appData = await AsyncStorage.getItem('isAppFirstLaunched');
+    if (appData == null) {
+      setIsAppFirstLaunched(true);
+      AsyncStorage.setItem('isAppFirstLaunched', 'false');
+    } else {
+      setIsAppFirstLaunched(false);
+    }
+
+    // AsyncStorage.removeItem('isAppFirstLaunched');
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    isAppFirstLaunched != null && (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          {isAppFirstLaunched && (
+            <Stack.Screen
+              name="OnboardingScreen"
+              component={OnboardingScreen}
+            />
+          )}
+          <Stack.Screen name="Anuncio1Screen" component={Anuncio1Screen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default  App;
---
+++ src/screens/Anuncio1Screen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+const Anuncio1Screen = () => {
+    return (
+    <SafeAreaView
+        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Login</Text>
+    </SafeAreaView>
+ );
+};
+
+export default Anuncio1Screen;
