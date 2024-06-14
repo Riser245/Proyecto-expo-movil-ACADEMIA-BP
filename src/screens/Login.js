1a7495222e986cc14dfe667dbe2b2b9aefd02edc
AnderFigueroa <20220105@ricaldone.edu.sv>
2024-06-14 15:37:48 -0600

    inicio login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import {ImageBackground, StyleSheet} from "react-native";
+
 const LoginScreen = () => {
     return (
-    <SafeAreaView
-        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Login</Text>
-    </SafeAreaView>
- );
-};
+      <ImageBackground
+        source={require('../imagenes/inicio.png')}
+        style={styles.backgroundImage}
+      >
+        {/* Contenido de tu componente */}
+      </ImageBackground>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    backgroundImage: {
+      flex: 1,
+      resizeMode: 'cover', // o 'contain' para ajustar la imagen al tamaño del contenedor
+    },
+  });
+  
 
 export default LoginScreen;
+ 
