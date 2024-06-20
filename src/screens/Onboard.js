a2e10fb2f6d9ac53d124646f37d15cefbad3dd3b
AnderFigueroa <20220105@ricaldone.edu.sv>
2024-06-20 11:00:34 -0600

    cambios pequeños

---
+++ src/screens/Onboard.js
@@ -0,0 +1,41 @@
+import 'react-native-gesture-handler'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import OnboardingScreen from './src/screens/OnboardingScreen';
+import LoginScreen from './src/screens/Login';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const stack = createStackNavigator();
+
+
+const Onboard = () => {
+  const [isAppFirstLaunched, setIsAppFirstLaunched] = React.useState(null);
+
+  React.useEffect(async () => {
+    const appData = await AsyncStorage.getItem('isAppFirstLaunched');
+    if (appData == null) {
+      setIsAppFirstLaunched(true);
+      AsyncStorage.setItem('isAppFirstLaunched', 'false');
+    } else {
+      setIsAppFirstLaunched(false);
+    }
+
+    // AsyncStorage.removeItem('isAppFirstLaunched');
+  }, []);
+  isAppFirstLaunched != null && (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        {isAppFirstLaunched && (
+          <Stack.Screen
+            name="OnboardingScreen"
+            component={OnboardingScreen}
+          />
+        )}
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          </Stack.Navigator>
+    </NavigationContainer>
+  )
+};
+
+export default Onboard;
--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -4,3 +4,4 @@
 //utilizar la direccion IP del servidor y no localhost
 export const IP = 'http://192.168.0.13';
 
+//192.168.1.11
