a333f951dfbf2a1b2d0838cfec0fb200e503832c
rebiiu <marhernanndezz@gmail.com>
2024-09-04 22:36:06 -0600

    Arreglo de navegavilidad

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -2,7 +2,7 @@
 //IP de donde se encuentra la Api, en este caso la dirección 
 //Ip de la maquina local 
 //utilizar la direccion IP del servidor y no localhost
-export const IP = 'http://192.168.0.5';
+export const IP = 'http://192.168.0.6';
 
 //Direcciones IP de todos
 
